1cffc0b1d54e423336ff6d9caa3b069650e32cec
Joe Axberg <jaxberg@umn.edu>
2025-01-24 16:52:08 -0600

    more code

---
+++ arrayofstructures.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct myStruct {
+  int myNum;
+  int myChar;
+
+};
+
+int main(){
+
+    struct myStruct arrayOfStructs[3];
+
+    //data for the first structure
+    arrayOfStructs[0].myChar='D';
+    arrayOfStructs[0].myNum=1;
+
+}
